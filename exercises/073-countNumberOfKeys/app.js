fd9ea85670c815419a403e60afa84150138f77f2
ricardoyepezz <ricardoyepezz@gmail.com>
2022-04-07 13:03:38 +0000

    exercise 75 done

--- exercises/073-countNumberOfKeys/app.js
+++ exercises/073-countNumberOfKeys/app.js
@@ -1,4 +1,12 @@
 function countNumberOfKeys(obj) {
-    // your code here
-
+  return Object.keys(obj).length;
 }
+
+var obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 5,
+};
+var output = countNumberOfKeys(obj);
+console.log(output); // --> 3
--- exercises/075-removeArrayValues/app.js
+++ exercises/075-removeArrayValues/app.js
@@ -1,3 +1,7 @@
 function removeArrayValues(obj) {
-    // your code here
+    for (let key in obj){
+        if (Array.isArray(obj[key])){
+            delete obj[key]
+        }
+    }return obj
 }
