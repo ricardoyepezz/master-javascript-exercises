698e4b19c67c515742e6a8b6a2e28358bc262d1f
ricardoyepezz <ricardoyepezz@gmail.com>
2022-08-26 19:00:45 +0000

    exercise 94 done

--- exercises/094-getFirstElementOfProperty/app.js
+++ exercises/094-getFirstElementOfProperty/app.js
@@ -1,1 +1,10 @@
 // Write your function here
+function getFirstElementOfProperty (obj,key){
+    return obj.key.shift()
+}
+
+var obj = {
+    key: [7, 2, 4]
+  };
+  var output = getFirstElementOfProperty(obj, 'key');
+  console.log(output);
